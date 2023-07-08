5ab666166934447d303d7c6c661fbcf9774babb1
crownsoft <130629588+whatsapp-filter@users.noreply.github.com>
2023-07-08 14:16:30 +0800

    Add files via upload

---
+++ Location.cs
@@ -0,0 +1,15 @@
+namespace WhatsAppNETAPI
+{
+	public class Location
+	{
+		public string latitude { get; set; }
+
+		public string longitude { get; set; }
+
+		public string description { get; set; }
+
+		public bool liveLocation { get; set; }
+
+		public string thumbnail { get; set; }
+	}
+}
